261b1a6b16062d0e80dc44d56b866417e37ea86e
AnastasiaDzam <yorklo@mail.ru>
2024-10-14 15:56:06 +0300

    checkpoint

--- padawans.js
+++ padawans.js
@@ -1,4 +1,36 @@
 const fs = require('fs')
+const {EOL} = require('os')
+
+function getPadawanNames (){
+const filePath = ('./data/padawans.txt');
+const readFile = fs.readFileSync(filePath, 'utf-8');
+return readFile.split(EOL).slice(0, -1);
+}
+
+function getLightsaberScores (){
+  const filePath = ('data/scores.txt');
+  const readFile = fs.readFileSync(filePath, 'utf-8').split(EOL);
+  return readFile.map( el => Number(el));
+  }
+
+  function getStats() {
+    const names = getPadawanNames();
+    const dates = getLightsaberScores();
+    const result = [];
+    for (let i = 0; i < names.length; i += 1) {
+      result.push([names[i], dates[i]]);
+    }
+    return result;
+  }
+
+    function writeStats() {
+      const result = getStats();
+      const filePath = ('data/stats.txt');
+      const firstResults = result.map((el) => el.join(" "));
+      const finalResults = firstResults.join("\n");
+      fs.writeFileSync(filePath, finalResults);
+    }
+
 module.exports = {
   getPadawanNames,
   getLightsaberScores,
